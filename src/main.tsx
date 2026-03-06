c8edcdd52246b9252bfcd8065c5481503cbd9506
wangyudeng-ai <wangyudeng3926832@gmail.com>
2026-03-06 23:30:29 +0800

    feat: load baidu analytics from env

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const baiduTongjiKey = import.meta.env.VITE_BAIDU_TONGJI_KEY as string | undefined;
+if (baiduTongjiKey) {
+  (window as any)._hmt = (window as any)._hmt || [];
+  const hm = document.createElement("script");
+  hm.src = `https://hm.baidu.com/hm.js?${baiduTongjiKey}`;
+  const s = document.getElementsByTagName("script")[0];
+  s.parentNode?.insertBefore(hm, s);
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
